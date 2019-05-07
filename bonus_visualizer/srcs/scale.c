b1f03a8bfabc5a4d94ce18d6f49a659235cfec6d
Anaelle LECLABART <anleclab@e2r6p16.42.fr>
2019-05-07 16:49:24 +0200

    fixed floating point exception error

--- bonus_visualizer/srcs/scale.c
+++ bonus_visualizer/srcs/scale.c
@@ -6,7 +6,7 @@
 /*   By: dtrigalo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/27 14:23:40 by dtrigalo          #+#    #+#             */
-/*   Updated: 2019/04/02 12:35:03 by dtrigalo         ###   ########.fr       */
+/*   Updated: 2019/05/07 16:48:31 by anleclab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,19 +38,27 @@ void	set_scale(t_lem *lem)
 {
 	int		tmp;
 
-	lem->scale = (HEIGHT - 2 * MARGIN) / (lem->y_max - lem->y_min);
-	tmp = (WIDTH - 2 * MARGIN) / (lem->x_max - lem->x_min);
-	if (tmp < lem->scale)
+	if (lem->y_max != lem->y_min)
+		lem->scale = (HEIGHT - 2 * MARGIN) / (lem->y_max - lem->y_min);
+	else
+		lem->scale = 0;
+	if (lem->x_max != lem->x_min)
+		tmp = (WIDTH - 2 * MARGIN) / (lem->x_max - lem->x_min);
+	else
+		tmp = 0;
+	if ((lem->scale == 0 && tmp) || tmp < lem->scale)
 	{
 		lem->scale = tmp;
 		lem->x_offset = MARGIN;
 		lem->y_offset = (HEIGHT - MARGIN - lem->scale * (lem->y_max
 					- lem->y_min)) / 2;
 	}
-	else
+	else if (tmp == 0 && lem->scale)
 	{
 		lem->y_offset = MARGIN;
 		lem->x_offset = (WIDTH - MARGIN - lem->scale * (lem->x_max
 					- lem->x_min)) / 2;
 	}
+	else
+		lem->scale = 1;
 }
